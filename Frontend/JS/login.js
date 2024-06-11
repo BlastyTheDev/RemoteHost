49d3ae3f28a5b2bf78e80c7e20359e2c8bdebbbf
Turtin <104549951+Turtin@users.noreply.github.com>
2024-06-11 17:59:50 +1000

    Main design of login page complete, Needs animations to be fixed, css infratructure complete

--- Frontend/JS/login.js
+++
@@ -1,7 +0,0 @@
-function hide() { 
-    document.getElementById('login').classList.toggle('hidden');
-    document.getElementById('signup').classList.toggle('hidden');
-}
-
-create.onclick = hide;
-cancel.onclick = hide;
