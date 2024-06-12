e9130c0aed7cbc883068b5fcb0c5652f5d4d90fc
BlastyTheDev <rmzha2234@gmail.com>
2024-06-12 18:55:44 +1000

    start maven project, move frontend to resources

---
+++ src/main/java/net/duckycraftmc/remotehost/RemoteHost.java
@@ -0,0 +1,13 @@
+package net.duckycraftmc.remotehost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteHost {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteHost.class, args);
+    }
+
+}
