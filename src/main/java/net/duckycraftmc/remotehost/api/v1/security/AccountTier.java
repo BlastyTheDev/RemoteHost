bdeabaf6e648cf28af0369952977e4f4d6732abd
BlastyTheDev <rmzha2234@gmail.com>
2024-06-19 18:03:43 +1000

    create user data class and SQL table

---
+++ src/main/java/net/duckycraftmc/remotehost/api/v1/security/AccountTier.java
@@ -0,0 +1,11 @@
+package net.duckycraftmc.remotehost.api.v1.security;
+
+public enum AccountTier {
+
+    BASIC,
+    ENHANCED,
+    ADVANCED,
+    FULL,
+    ADMIN
+
+}
---
+++ src/main/java/net/duckycraftmc/remotehost/api/v1/security/User.java
@@ -0,0 +1,45 @@
+package net.duckycraftmc.remotehost.api.v1.security;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    private String discord;
+    @Column(nullable = false)
+    private AccountTier tier;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(tier.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
