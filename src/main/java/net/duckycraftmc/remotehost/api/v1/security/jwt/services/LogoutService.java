50c584328d98328353c6d845b9e118f36ea4fb75
BlastyTheDev <rmzha2234@gmail.com>
2024-06-28 17:36:52 +1000

    create logoutservice

---
+++ src/main/java/net/duckycraftmc/remotehost/api/v1/security/jwt/services/LogoutService.java
@@ -0,0 +1,24 @@
+package net.duckycraftmc.remotehost.api.v1.security.jwt.services;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            return;
+
+        String jwt = authHeader.split(" ")[1].trim();
+        // implement revoking of tokens in SQL database
+    }
+
+}
